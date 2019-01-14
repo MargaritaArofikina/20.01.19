64ef1673744544863aa90afa422adccf85948fa1
MargaritaArofikina <44144804+MargaritaArofikina@users.noreply.github.com>
2019-01-14 21:46:50 +0300

    Update and rename 04!!.cpp to 04.cpp

--- 04.cpp
+++ 04.cpp
@@ -5,31 +5,6 @@
 #include <iostream>
 using namespace std;
 
-double root2(int n) {    //квадратный корень  
-
-	double m;                         
-	double a = 0.00001;
-	for (double i = 0; ; i += a){
-		if (((i*i) <= n) && (((i + a)*(i + a)) >= n)) {
-			m = i;
-			break;
-		}
-	}
-	m *= (1 / a);
-	long long s = (long long)m;
-	int e = (s % 10);
-	if (e >= 5){
-		s -= e;
-		s += 10;
-	}
-	else {
-		s -= e;
-	}
-	m = (s / 10);
-	m *= (a * 10);
-
-	return m;
-}
 
 int main (){
 
@@ -39,21 +14,27 @@ int *k = new int [Y + 1];
 
 
 for (int i = 0; i <= Y; i++){
+
+bool sqrt;  //проверка, является ли число полным квадратом
+int j;
+for (j = 1; j <= i; j++){
+  if (j*j == i) {sqrt = 1;
+  break;
+  }
+}
+if (j > i) sqrt = 0;
+
   if (i == X){
     *(k + i) = 1;
   } else if (i < X){
     *(k + i) = 0;
-  } else if ((i%3 == 0) && (root2(i)%1 == 0)) {
-    double j;
-    j = i;
-    *(k + i) = *(k + i - 5) + *(k + i/3) + *(k + root2(j)) + *(k + (i - 2)/10);
-  } else if ((i % 3 == 0) && (root2(i)%1 != 0)){
+  } else if ((i%3 == 0) && (sqrt == 1)) {
+    *(k + i) = *(k + i - 5) + *(k + i/3) + *(k + j) + *(k + (i - 2)/10);
+  } else if ((i % 3 == 0) && (sqrt == 0)){
     *(k + i) = *(k + i - 5) + *(k + i/3) + *(k + (i - 2)/10);
-  } else if ((i % 3 != 0) && (root2(i)%1 == 0)){
-     double j;
-    j = i;
-    *(k + i) = *(k + i - 5) + *(k + (i - 2)/10) +  *(k + root2(j));
-  } else if ((i % 3 != 0)&&(root2(i)%1 !=0)){
+  } else if ((i % 3 != 0) && (sqrt == 1)){
+    *(k + i) = *(k + i - 5) + *(k + (i - 2)/10) +  *(k + j);
+  } else if ((i % 3 != 0)&&(sqrt == 0)){
     *(k + i) = *(k + i - 5) + *(k + (i - 2)/10);
   }
 }
@@ -61,7 +42,7 @@ for (int i = 0; i <= Y; i++){
 int s = *(k + Y) - *(k + Z);
 
 if (s > 0) cout << "YES";
-if (s < 0) cout << "NO";
+if (s <= 0) cout << "NO";
 
 return 0;
 }
